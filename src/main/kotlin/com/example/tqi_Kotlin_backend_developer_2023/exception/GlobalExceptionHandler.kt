363a47168f1eead3ecd21f8b2ce7bdf0d6ff807e
guifx <guifxb@yahoo.com>
2023-07-20 14:32:48 -0300

    Added PostgreSQL container, profiles setup and implementation for customer and category restControllers

--- src/main/kotlin/com/example/tqi_Kotlin_backend_developer_2023/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/tqi_Kotlin_backend_developer_2023/exception/GlobalExceptionHandler.kt
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.client.HttpServerErrorException.InternalServerError
 import java.sql.SQLIntegrityConstraintViolationException
 
 
